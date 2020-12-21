9b18b17c6b2006a36334fefa0c1d0a66e801e490
Rafael Mendes <mendesrafa.c@gmail.com>
2020-12-21 20:09:33 -0300

    initial commit

---
+++ src/db/index.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+import { Pool } from 'pg'
+
+dotenv.config()
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+})
+
+export default db
